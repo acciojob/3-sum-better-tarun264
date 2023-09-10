016f387f91e1d496ddc0033c69769f1d8a2abf2d
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-10 23:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function threeSum(arr, target) {
-// write your code here
+arr.sort((a,b)=> a-b );
+	let closestsum= Infinity;
+
+	for(let i=0;i<arr.length-2;i++){
+		if(i!=0 && arr[i]==arr[i-1])continue;
+		let j=i+1;
+		let k=arr.length-1;
+		while(j<k){
+			sum=arr[i]+arr[j]+arr[k];
+			if(Math.abs(sum-target) < Math.abs(closestsum-target))
+				closestsum=sum;
+
+			while(j<k && arr[j-1]!=arr[j]) j++;
+			while(k>j && arr[k+1]!=arr[k]) k--;
+
+			if(sum>target) k--;
+			else j++;
+		}
+		
+	}
+	return closestsum;
   
 }
 
